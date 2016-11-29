38e6c979629e57a33b4f04f42ef48a752aafce32
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-11-29 18:03:17 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,23 @@
 #!/bin/sh -e
 
+# value
+CONFIGFILE=/config/qBittorrent.conf
 # Default configuration file
-if [ ! -f /config/qBittorrent.conf ]
+if [ ! -f $CONFIGFILE ]
 then
-	cp /default/qBittorrent.conf /config/qBittorrent.conf
+cat <<EOF>> $CONFIGFILE
+[Preferences]
+General\Locale=C
+WebUI\Enabled=true
+Downloads\SavePath=/downloads
+Connection\PortRangeMin=6881
+
+[LegalNotice]
+Accepted=true
+
+[General]
+ported_to_new_savepath_system=true
+EOF
 fi
 
 # Allow groups to change files.
